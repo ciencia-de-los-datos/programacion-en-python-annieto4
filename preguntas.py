88c8d39a9227e61eaab2edf80527cb340045eda5
annieto4 <101904204+annieto4@users.noreply.github.com>
2022-03-23 19:53:09 -0500

    Update preguntas.py
    Solución de pregunta #1

--- preguntas.py
+++ preguntas.py
@@ -11,7 +11,12 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
+import csv
 
+with open("data.csv", newline='') as f:
+    datos = csv.reader(f, delimiter='\t')
+    colums = list(datos)
+        
 
 def pregunta_01():
     """
@@ -19,9 +24,13 @@ def pregunta_01():
 
     Rta/
     214
-
     """
-    return
+
+    suma = 0
+    for num in colums:
+        suma += int(num[1])
+
+    return suma
 
 
 def pregunta_02():
